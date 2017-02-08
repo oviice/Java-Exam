27e2ad0373fa8ff5afdd9df970ec4e5fc8f30c9a
Ovi <oviice@gmail.com>
2017-02-08 16:39:31 +0600

    Java Exception

---
+++ Java Inheritance/src/Calculator.java
@@ -0,0 +1,23 @@
+/**
+ * Created by Ovi on 2/8/2017.
+ */
+public class Calculator {
+    double z;
+    public void add(int x,int y){
+        z=x+y;
+        System.out.println("The Addition value is : "+z);
+    }
+    public void subtract(int x,int y){
+        z=x-y;
+        System.out.println("The Subtraction value is : "+z);
+    }
+    public void multiply(int x,int y){
+        z=x*y;
+        System.out.println("The Subtraction value is : "+z);
+    }
+    public void divide(int x,int y){
+        z=x/y;
+        System.out.println("The Subtraction value is : "+z);
+    }
+
+}
---
+++ Java Inheritance/src/JavaInheritance.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+/**
+ * Created by Ovi on 2/8/2017.
+ */
+public class JavaInheritance extends Calculator {
+    public static void main(String args[]){
+        int a,b;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Please input the number of a : ");
+        a=sc.nextInt();
+        System.out.print("Please input the number of b: ");
+        b=sc.nextInt();
+        JavaInheritance inheritance=new JavaInheritance();
+        inheritance.add(a,b);
+        inheritance.subtract(a,b);
+        inheritance.multiply(a,b);
+        inheritance.divide(a,b);
+    }
+}
