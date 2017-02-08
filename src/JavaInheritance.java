f26a12800e0138bd851fb8d3f0dea5a42c6f1540
Ovi <oviice@gmail.com>
2017-02-08 15:49:34 +0600

    Java Inheritance

--- src/JavaInheritance.java
+++ src/JavaInheritance.java
@@ -7,9 +7,9 @@ public class JavaInheritance extends Calculator {
     public static void main(String args[]){
         int a,b;
         Scanner sc=new Scanner(System.in);
-        System.out.print("Please input the number of a : ");
+        System.out.print("Please input the number of a: ");
         a=sc.nextInt();
-        System.out.print("Please input the number of b : ");
+        System.out.print("Please input the number of b: ");
         b=sc.nextInt();
         JavaInheritance inheritance=new JavaInheritance();
         inheritance.add(a,b);
